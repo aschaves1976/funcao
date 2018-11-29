eabaaf86359d6e2986ec6dc87269d76acd9a5bf2
aschaves1976 <roots17@gmail.com>
2018-11-28 23:03:46 -0200

    Função Factory #01

---
+++ factory01.js
@@ -0,0 +1,9 @@
+// Função factory é uma função que retorna um objeto
+
+function criarPessoa(){
+    return {
+        nome: 'Maria',
+        sobrenome: 'Silva'
+    }
+}
+console.log(criarPessoa())
