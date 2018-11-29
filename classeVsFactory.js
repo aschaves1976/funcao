14bb1b0441e713a56100494efe6c89f3d9647aff
aschaves1976 <roots17@gmail.com>
2018-11-28 23:36:59 -0200

    Classe Versus Função Factory

---
+++ classeVsFactory.js
@@ -0,0 +1,21 @@
+class Pessoa{
+    constructor(nome){
+        this.nome = nome
+    }
+
+    falar(){
+        console.log(`Olá, meu nome é ${this.nome}`)
+    }
+}
+
+const p1 = new Pessoa('João')
+p1.falar()
+
+// Melhor forma de trabalhar
+const criarPessoa = nome =>{
+    return {
+        falar: () => console.log(`Olá, meu nome é ${nome}`)
+    }
+}
+const p2 = criarPessoa('Maria')
+p2.falar()
