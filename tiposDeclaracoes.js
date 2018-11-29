7c571bea7163e065264ac11316ba376d3ff23a0b
aschaves1976 <roots17@gmail.com>
2018-11-28 22:57:08 -0200

    Closures

---
+++ tiposDeclaracoes.js
@@ -0,0 +1,14 @@
+//function declaration - forma tradicional
+function soma (x, y){
+    return x + y
+}
+
+// function expression - criar uma funcao anonima e atribuir a uma variavel ou constante
+const sub = function(x, y){
+    return x - y
+}
+
+//named function expression
+const mult = function mult(x, y){
+    return x * Y
+}
